256cd6be3c7de1154fd9e7c9befe2c719a913f68
iqbalfachry19 <ifachry@rocketmail.com>
2022-10-29 14:22:09 +0700

    fix

--- app/generate/Generate.tsx
+++ app/generate/Generate.tsx
@@ -1,18 +1,37 @@
 'use client';
 import { useState } from 'react';
-import QRCode from 'react-qr-code';
+import QRCode from 'qrcode.react';
 import styles from '../page.module.css';
 type Props = {};
 
 const Generate = (props: Props) => {
   const [qrCodeValue, setQrCodeValue] = useState('');
 
+  const downloadQRCode = () => {
+    // Generate download with use canvas and stream
+    const canvas = document.getElementById('qr-gen') as HTMLCanvasElement;
+    const pngUrl = canvas
+      .toDataURL('image/png')
+      .replace('image/png', 'image/octet-stream');
+    let downloadLink = document.createElement('a');
+    downloadLink.href = pngUrl;
+    downloadLink.download = `${qrCodeValue}.png`;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
   return (
     <>
       <div className={styles.card}>Generate QR</div>
 
       {qrCodeValue != '' && (
-        <QRCode value={qrCodeValue} className={styles.containerColumn} />
+        <QRCode
+          id="qr-gen"
+          value={qrCodeValue}
+          size={290}
+          level={'H'}
+          includeMargin={true}
+        />
       )}
       <input
         className={styles.card}
@@ -20,6 +39,12 @@ const Generate = (props: Props) => {
           setQrCodeValue(e.target.value);
         }}
       />
+      <p>
+        Click for{' '}
+        <button type="button" onClick={downloadQRCode}>
+          Download QR Code
+        </button>
+      </p>
     </>
   );
 };
