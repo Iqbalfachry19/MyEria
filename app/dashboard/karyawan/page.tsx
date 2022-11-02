c9a42046f9f24f9524efced77a06bdf379a25fad
iqbalfachry19 <ifachry@rocketmail.com>
2022-11-02 10:56:52 +0700

    hi

--- app/dashboard/karyawan/page.tsx
+++ app/dashboard/karyawan/page.tsx
@@ -1,7 +1,11 @@
 import Karyawan from './Karyawan';
 import Tambah from './tambah/Tambah';
 export const dynamic = 'auto',
-  revalidate = 0;
+  dynamicParams = true,
+  revalidate = 0,
+  fetchCache = 'auto',
+  runtime = 'nodejs',
+  preferredRegion = 'auto';
 function Page() {
   return (
     <div className="flex flex-col h-screen justify-center items-center">
