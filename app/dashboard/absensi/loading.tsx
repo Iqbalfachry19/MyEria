0a9eba613dbf52aa9435813d90bb8c0ac2082fa6
iqbalfachry19 <ifachry@rocketmail.com>
2022-11-05 03:12:09 +0700

    hi

--- app/dashboard/absensi/loading.tsx
+++ app/dashboard/absensi/loading.tsx
@@ -2,7 +2,7 @@ import { BeatLoader } from 'react-spinners';
 
 export default function Loading() {
   return (
-    <div className="flex">
+    <div className="flex flex-col justify-center items-center h-screen">
       <BeatLoader color="#36d7b7" />
     </div>
   );
