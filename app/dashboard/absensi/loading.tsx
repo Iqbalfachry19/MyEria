1b331ea33dcc57b8449facdaa8080dae4f1b3b21
iqbalfachry19 <ifachry@rocketmail.com>
2022-11-05 03:29:21 +0700

    hi

--- app/dashboard/absensi/loading.tsx
+++ app/dashboard/absensi/loading.tsx
@@ -1,6 +1,6 @@
 import { SpinnerCircular } from 'spinners-react';
-import '~spinners-react/lib/SpinnerCircular.css';
-import '~spinners-react/lib/index.css';
+import 'spinners-react/lib/SpinnerCircular.css';
+
 export default function Loading() {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
