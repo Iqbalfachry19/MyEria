93f67bc9a3699207ec9198d18cb7cf3625a9e2dc
iqbalfachry19 <ifachry@rocketmail.com>
2022-11-05 19:25:02 +0700

    hI

--- app/dashboard/karyawan/(edit)/layout.tsx
+++ app/dashboard/karyawan/(edit)/layout.tsx
@@ -4,16 +4,10 @@ import Karyawan from './Karyawan';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative  z-20">
-      <nav className="space-x-2 flex  p-2 text-white absolute w-full bg-gray-600">
-        <Link href="/dashboard/karyawan">list Karyawan</Link>
-        <Link href="/dashboard/karyawan/tambah">Tambah karyawan</Link>
-      </nav>
-      <div className="h-screen flex flex-col  justify-center items-center">
-        {/* @ts-ignore */}
-        <Karyawan />
-        <div>{children}</div>
-      </div>
+    <div className="h-screen flex flex-col  justify-center items-center">
+      {/* @ts-ignore */}
+      <Karyawan />
+      <div>{children}</div>
     </div>
   );
 }
