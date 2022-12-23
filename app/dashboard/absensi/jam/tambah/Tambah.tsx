1090aa49a805121a04d5c8fcc31c43a13734e549
iqbalfachry19 <ifachry@rocketmail.com>
2022-12-23 09:48:04 +0700

    hi

--- app/dashboard/absensi/jam/tambah/Tambah.tsx
+++ app/dashboard/absensi/jam/tambah/Tambah.tsx
@@ -4,7 +4,7 @@ import { TextField } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { Suspense, useState } from 'react';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs, { Dayjs } from 'dayjs';
 const Tambah = ({ posts }: any) => {
@@ -29,48 +29,50 @@ const Tambah = ({ posts }: any) => {
   };
   return (
     <div className="flex px-2">
-      <form onSubmit={(e) => create(e)} className="">
-        <h1>Tambah Jam Absensi Karyawan</h1>
-        <div className="flex flex-col">
-          <label>Karyawan</label>
-          <select
-            value={selectedOption}
-            onChange={(e) => setSelectedOption(e.target.value)}
-          >
-            {datas.map((post: any) => (
-              <option key={post.id} value={post.id}>
-                {post.nama}
-              </option>
-            ))}
-          </select>
-          <div className="flex flex-col mt-10">
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <TimePicker
-                label="Jam Keluar"
-                value={jamMasuk}
-                onChange={(newValue) => {
-                  setJamMasuk(newValue);
-                }}
-                renderInput={(params) => <TextField {...params} />}
-              />
-            </LocalizationProvider>
+      <Suspense>
+        <form onSubmit={(e) => create(e)} className="">
+          <h1>Tambah Jam Absensi Karyawan</h1>
+          <div className="flex flex-col">
+            <label>Karyawan</label>
+            <select
+              value={selectedOption}
+              onChange={(e) => setSelectedOption(e.target.value)}
+            >
+              {datas.map((post: any) => (
+                <option key={post.id} value={post.id}>
+                  {post.nama}
+                </option>
+              ))}
+            </select>
+            <div className="flex flex-col mt-10">
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <TimePicker
+                  label="Jam Keluar"
+                  value={jamMasuk}
+                  onChange={(newValue) => {
+                    setJamMasuk(newValue);
+                  }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+            </div>
+            <div className="flex flex-col mt-10">
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <TimePicker
+                  label="Jam Keluar"
+                  value={jamKeluar}
+                  onChange={(newValue) => {
+                    console.log(newValue);
+                    setJamKeluar(newValue);
+                  }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+            </div>
+            <button type="submit">Tambah Jam Absensi Karyawan</button>
           </div>
-          <div className="flex flex-col mt-10">
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <TimePicker
-                label="Jam Keluar"
-                value={jamKeluar}
-                onChange={(newValue) => {
-                  console.log(newValue);
-                  setJamKeluar(newValue);
-                }}
-                renderInput={(params) => <TextField {...params} />}
-              />
-            </LocalizationProvider>
-          </div>
-          <button type="submit">Tambah Jam Absensi Karyawan</button>
-        </div>
-      </form>
+        </form>
+      </Suspense>
     </div>
   );
 };
