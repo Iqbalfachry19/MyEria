6f866d94b1c8387a0232a5954610d90146101995
iqbalfachry19 <ifachry@rocketmail.com>
2023-01-08 09:26:55 +0700

    hi

--- pages/api/createKaryawan.ts
+++ pages/api/createKaryawan.ts
@@ -39,6 +39,6 @@ export default async function handler(
     });
     res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ message: 'Error connecting to db', err });
   }
 }
