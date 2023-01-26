9b71542797f8d9608b5f4a77ac27fa390b3233b2
iqbalfachry19 <ifachry@rocketmail.com>
2023-01-26 21:22:46 +0700

    hi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,18 @@
 import '../styles/globals.css';
 import 'spinners-react/lib/SpinnerCircular.css';
-
-export default function RootLayout({
+import { unstable_getServerSession } from 'next-auth';
+import Providers from './Providers';
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await unstable_getServerSession();
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers session={session}>{children}</Providers>
+      </body>
     </html>
   );
 }
