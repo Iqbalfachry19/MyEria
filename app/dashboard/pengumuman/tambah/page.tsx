3126738a17e3656281d53280f542558134090fe1
iqbalfachry19 <ifachry@rocketmail.com>
2023-01-28 00:20:45 +0700

    hi

--- app/dashboard/pengumuman/tambah/page.tsx
+++ app/dashboard/pengumuman/tambah/page.tsx
@@ -1,17 +1,9 @@
 import Tambah from './Tambah';
-const getData = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/getKaryawan`, {
-    cache: 'no-store',
-  });
-  const posts = await res.json();
-  return posts as any[];
-};
-async function Page() {
-  const posts = await getData();
 
+async function Page() {
   return (
     <div className="flex flex-col w-screen h-screen justify-center items-center">
-      <Tambah posts={posts} />
+      <Tambah />
     </div>
   );
 }
