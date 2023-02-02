7edb3866f4db319fa68f6fbd9fee0326b9aac73f
iqbalfachry19 <ifachry@rocketmail.com>
2023-02-02 12:47:04 +0700

    fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,11 @@
-function Page() {
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '../../pages/api/auth/[...nextauth]';
+async function Page() {
+  const session = await getServerSession(authOptions);
+
   return (
     <div className="flex flex-col h-screen justify-center items-center">
-      <div>hi Selamat Datang di dashboard MyEria</div>
+      <div>hi {session?.user?.name} Selamat Datang di dashboard MyEria</div>
     </div>
   );
 }
