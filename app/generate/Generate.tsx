4dd738226857cc81a5f55906bc83deccc7b3e8e3
iqbalfachry19 <ifachry@rocketmail.com>
2023-02-02 17:55:19 +0700

    hi

--- app/generate/Generate.tsx
+++ app/generate/Generate.tsx
@@ -1,12 +1,13 @@
 'use client';
 import { useRef, useState } from 'react';
 import QRCode from 'qrcode.react';
+import Image from 'next/image';
 
 type Props = {};
 
 const Generate = (props: Props) => {
   const [qrCodeValue, setQrCodeValue] = useState('');
-
+  const [lokasi, setLokasi] = useState('');
   const downloadQRCode = () => {
     // Generate download with use canvas and stream
     const canvas = document.getElementById('qr-gen') as HTMLCanvasElement;
@@ -29,25 +30,53 @@ const Generate = (props: Props) => {
     }
   };
   return (
-    <>
+    <div className="flex flex-col  h-screen">
       <div className="border-2  mx-auto mt-10 hover:text-blue-400  hover:border-blue-400 py-4 transition-all duration-200 ease-out rounded-lg px-4">
         Generate QR
       </div>
-
-      {qrCodeValue != '' && (
-        <QRCode
-          id="qr-gen"
-          value={qrCodeValue}
-          size={290}
-          level={'H'}
-          includeMargin={true}
-        />
-      )}
+      <div className="flex mt-10">
+        <div
+          id="bg"
+          className="bg-cover  flex"
+          style={{
+            backgroundImage: `url('/bg.png')`,
+            width: '21cm',
+            height: '29.7cm',
+          }}
+        >
+          {qrCodeValue != '' && (
+            <>
+              <QRCode
+                id="qr-gen"
+                value={qrCodeValue}
+                size={300}
+                level={'H'}
+                includeMargin={true}
+                style={{
+                  zIndex: 2,
+                  position: 'absolute',
+                  margin: '12cm 6.6cm',
+                }}
+                className=""
+              />
+              <div className="w-20 h-20 absolute z-10 mt-[16cm] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <Image src="/logo.png" className="" fill alt="" />
+              </div>
+              <div className="relative mx-72 inset-x-0">
+                <p className="z-5 text-[#373737] font-sans text-4xl absolute   font-bold my-[21cm]">
+                  {lokasi}
+                </p>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
       <h1 className="mt-10">Lokasi</h1>
       <input
         className="outline-none ring-2 ring-gray-100 rounded-lg h-20 p-2 hover:ring-blue-400"
         onChange={(e) => {
           handleInputChange(e);
+          setLokasi(e.target.value);
         }}
       />
 
@@ -57,7 +86,7 @@ const Generate = (props: Props) => {
           Download QR Code
         </button>
       </p>
-    </>
+    </div>
   );
 };
 
