96e18800a90e09b8c95a26a0f503732a02c5f685
iqbalfachry19 <ifachry@rocketmail.com>
2023-02-04 09:18:32 +0700

    hi

--- db/auth.ts
+++ db/auth.ts
@@ -1,23 +1,71 @@
-import * as z from 'zod';
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import Credentials from 'next-auth/providers/credentials';
+import { verify } from 'argon2';
+import { NextAuthOptions } from 'next-auth';
+import { prisma } from '../db/db';
+import { loginSchema } from '../db/validation/auth';
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    Credentials({
+      id: 'password',
+      name: 'username',
+      credentials: {
+        username: {
+          label: 'Username',
+          type: 'text',
+        },
+        password: { label: 'Password', type: 'password' },
+      },
+      authorize: async (credentials, req) => {
+        const creds = await loginSchema.parseAsync(credentials);
 
-export const loginSchema = z.object({
-  username: z.string(),
-  password: z.string().min(3).max(12),
-});
+        const user: any = await prisma.karyawan.findFirst({
+          where: { username: creds.username },
+        });
 
-export const signUpSchema = loginSchema
-  .extend({
-    username: z.string(),
-    confirmPassword: z.string().min(4).max(12),
-  })
-  .superRefine(({ confirmPassword, password }, ctx) => {
-    if (confirmPassword !== password)
-      ctx.addIssue({
-        path: ['confirmPassword'],
-        code: 'custom',
-        message: 'The passwords did not match',
-      });
-  });
+        if (user) {
+          const isValidPassword = await verify(user.password, creds.password);
+          if (isValidPassword) {
+            return {
+              id: user.id,
 
-export type ILogin = z.infer<typeof loginSchema>;
-export type ISignUp = z.infer<typeof signUpSchema>;
+              name: user.nama,
+            };
+          }
+        }
+        return null;
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.id = user.id;
+        token.name = user.name;
+      }
+
+      return token;
+    },
+    session: async ({ session, token }) => {
+      if (token) {
+        session.id = token.id;
+        session.name = token.name;
+      }
+
+      return session;
+    },
+  },
+  jwt: {
+    maxAge: 15 * 24 * 30 * 60, // 15 days
+  },
+  secret: process.env.NEXTAUTH_SECRET!,
+  pages: {
+    signIn: '/auth/signin',
+  },
+};
