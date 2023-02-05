deb6e6273519a10a9b13926ea68c05d1da01e921
iqbalfachry19 <ifachry@rocketmail.com>
2023-02-05 19:39:36 +0700

    fix

--- app/dashboard/karyawan/(edit)/Karyawan.tsx
+++ app/dashboard/karyawan/(edit)/Karyawan.tsx
@@ -15,7 +15,7 @@ const Karyawan = async (props: Props) => {
   const posts = await getData();
 
   return (
-    <div className="flex flex-col justify-center bg-white rounded-lg p-4 mx-auto items-center  w-10/11">
+    <div className="flex flex-col justify-center bg-white rounded-lg p-4 mx-auto items-center  ">
       <Table posts={posts} />
     </div>
   );
