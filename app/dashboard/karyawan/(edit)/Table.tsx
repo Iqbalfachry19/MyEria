d568fc280037cedd21c76e27b6790ce6f443a76e
iqbalfachry19 <ifachry@rocketmail.com>
2023-02-05 19:43:51 +0700

    fix

--- app/dashboard/karyawan/(edit)/Table.tsx
+++ app/dashboard/karyawan/(edit)/Table.tsx
@@ -77,7 +77,7 @@ function Table({ posts }: any) {
     getCoreRowModel: getCoreRowModel(),
   });
   return (
-    <div className="h-2/3">
+    <div className="h-[28rem]">
       <h1>List Karyawan</h1>
       <div className="overflow-y-scroll flex-col flex ">
         <table className="border-2  border-black ">
