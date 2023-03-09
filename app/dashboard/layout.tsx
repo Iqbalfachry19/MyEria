663e1f070a5bcd1e6b1126ff26858132d09dd168
iqbalfachry19 <ifachry@rocketmail.com>
2023-03-09 17:11:53 +0700

    hi

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -10,28 +10,29 @@ export default async function Layout({
   children: React.ReactNode;
 }) {
   const session = await getServerSession(authOptions);
+
   return (
-    <div className="flex w-full h-screen z-10 relative ">
-      <nav className="space-y-2 flex flex-col p-2 	 border-r border-black shadow-2xl    text-white  bg-gray-600">
+    <div className="flex w-full h-screen z-10 relative">
+      <nav className="space-y-2 flex flex-col p-2 border-r border-black shadow-2xl text-white bg-gray-600">
         <div className="h-10 w-10 relative">
           <Image
-            src={`${
+            src={
               session?.user?.image
                 ? session.user.image
                 : `https://avatars.dicebear.com/api/initials/${session?.user?.name}.svg`
-            }`}
+            }
             alt=""
             fill
           />
         </div>
         <p>{session?.user?.name}</p>
-        <Link href="/dashboard">home</Link>
-        <Link href="/dashboard/karyawan">karyawan</Link>
+        <Link href="/dashboard">Home</Link>
+        <Link href="/dashboard/karyawan">Karyawan</Link>
         <Link href="/dashboard/absensi">Absensi</Link>
         <Link href="/dashboard/pengumuman">Pengumuman</Link>
         <LogoutButton />
       </nav>
-      <div className="w-full ">{children}</div>
+      <div className="w-full">{children}</div>
     </div>
   );
 }
