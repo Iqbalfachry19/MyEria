6eeb011821e7c22b6e55d534f34ffe644c230d91
iqbalfachry19 <ifachry@rocketmail.com>
2023-03-12 10:24:24 +0700

    fix

--- app/dashboard/absensi/(edit)/layout.tsx
+++ app/dashboard/absensi/(edit)/layout.tsx
@@ -9,17 +9,25 @@ const getData = async () => {
   const posts = await res.json();
   return posts as any[];
 };
+const getKaryawan = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/getKaryawan`, {
+    cache: 'no-store',
+  });
+  const posts = await res.json();
+  return posts as any[];
+};
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   const posts = await getData();
+  const karyawan = await getKaryawan();
   return (
     <div className="flex flex-row overflow-y-scroll h-screen justify-center items-center">
       <div className="flex h-5/6  ">
         <Suspense fallback={<SpinnerCircular />}>
-          <Absensi posts={posts} />
+          <Absensi posts={posts} karyawan={karyawan} />
         </Suspense>
       </div>
       <div className="flex ml-2 bg-white">{children}</div>
