8116ec25768453a6dab5133acfbfe3adeff28358
iqbalfachry19 <ifachry@rocketmail.com>
2023-03-15 14:53:39 +0700

    fix

--- app/dashboard/absensi/(edit)/edit/[id]/Ubah.tsx
+++ app/dashboard/absensi/(edit)/edit/[id]/Ubah.tsx
@@ -50,30 +50,35 @@ const Ubah = ({ params, post }: any) => {
           type="text"
           placeholder="Pukul"
           value={pukul}
+          className="border border-gray-400 rounded-lg p-2 "
           onChange={(e) => setPukul(e.target.value)}
         />
         <input
           type="text"
           placeholder="Tanggal"
           value={tanggal}
+          className="border border-gray-400 rounded-lg p-2 mt-2"
           onChange={(e) => setTanggal(e.target.value)}
         />
         <input
           type="text"
           placeholder="Lokasi"
           value={lokasi}
+          className="border border-gray-400 rounded-lg p-2 mt-2"
           onChange={(e) => setLokasi(e.target.value)}
         />
         <input
           type="text"
           placeholder="Status"
           value={status}
+          className="border border-gray-400 rounded-lg p-2 mt-2"
           onChange={(e) => setStatus(e.target.value)}
         />
         <input
           type="text"
           placeholder="Keterangan"
           value={keterangan}
+          className="border border-gray-400 rounded-lg p-2 mt-2"
           onChange={(e) => setKeterangan(e.target.value)}
         />
         <button
