7d5768118c42da6a3c2736995f2f2509f1a023aa
iqbalfachry19 <ifachry@rocketmail.com>
2023-03-15 15:22:11 +0700

    fix

--- app/generate/layout.tsx
+++ app/generate/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="bg-gray-100">
       {' '}
       <nav className="space-x-2 flex  p-2 text-white absolute w-full items-center justify-between bg-gray-600">
-        <Link href="/">
+        <Link className="flex" href="/">
           <ArrowLeftIcon className="w-4 h-4" /> <span>Kembali</span>
         </Link>
         <Link href="/dashboard">Dashboard</Link>
